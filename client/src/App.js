ee5721d954ae9379ee75ee72a12f50c9366c72e3
Teddy Milford <teddy.milford@gmail.com>
2022-07-11 13:19:53 -0600

    merge fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import HomeNav from "./HomeNav";
-import Card from "./Card";
 
 function App() {
-  return (
-    <div className="App">
-      <HomeNav />
-      <Card />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
