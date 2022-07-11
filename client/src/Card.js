e7a45e75fafab7012ac2166956967f559d0b197e
Teddy Milford <teddy.milford@gmail.com>
2022-07-11 13:14:46 -0600

    card

---
+++ client/src/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
+      <img
+        className="w-full"
+        src="https://www.artic.edu/iiif/2/ded2fc0d-564a-670a-178e-5acc0af5d823/full/843,/0/default.jpg"
+        alt="Sunset in the mountains"
+        // width={4}
+      ></img>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
