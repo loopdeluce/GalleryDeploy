701d1f7b28e4e0d5d62c0f3255ba1ae635cf497f
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-08-15 16:47:18 -0600

    artwork pull updated

--- server/app/models/Artwork.rb
+++ server/app/models/Artwork.rb
@@ -3,7 +3,7 @@ class Artwork < ActiveRecord::Base
   has_many :users, through: :artworks
 
   def self.fetchOneThousandRandomArtworks
-    self.all.shuffle.take(250)
+    self.all.shuffle.take(100)
   end
 end
 
