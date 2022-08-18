c99fd4a5fb6765504335362112a504e57511a209
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-08-18 16:27:45 -0600

    enironment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter  =>  'postgresql',
-  :database => 'Gallery_production',
-  :encoding => 'utf8'
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter  =>  'postgresql',
+#   :database => 'Gallery_development',
+#   :encoding => 'utf8'
+# )
 
 
 require './app/controllers/application_controller'
